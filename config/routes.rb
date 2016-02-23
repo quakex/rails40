75027f38daa05aa126fdc85037a570ec35755e74
quakex <iquakex@gmail.com>
2016-02-23 20:10:59 +0800

    Task C, Iteration C2:Adding a Page Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
+
+  root 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
